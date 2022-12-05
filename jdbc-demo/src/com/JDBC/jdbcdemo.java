9c05e34259b2b97a72bf45432e0c19b276111e77
Jiuyao Qu <2668136485@qq.com>
2022-12-05 18:18:00 +0800

    test1

---
+++ jdbc-demo/src/com/JDBC/jdbcdemo.java
@@ -0,0 +1,28 @@
+package com.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class jdbcdemo {
+    public static void main(String[] args) throws Exception {
+        //1. 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        //2. 获取连接
+        String url = "jdbc:mysql://127.0.0.1:3306/db1";
+        String username = "root";
+        String password = "1234";
+        Connection conn = DriverManager.getConnection(url, username, password);
+        //3. 定义sql
+        String sql = "update account set money = 2000 where id = 1";
+        //4. 获取执行sql的对象 Statement
+        Statement stmt = conn.createStatement();
+        //5. 执行sql
+        int count = stmt.executeUpdate(sql);//受影响的行数
+        //6. 处理结果
+        System.out.println(count);
+        //7. 释放资源
+        stmt.close();
+        conn.close();
+    }
+}
